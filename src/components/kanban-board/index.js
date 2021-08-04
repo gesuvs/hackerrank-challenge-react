374c690417df236cdd72c2a5ba4fcc580b2fcbf1
gesuvs <g_bernardo10@hotmail.com>
2021-08-04 20:25:47 +0000

    fix
    Commit by HackerRank

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -86,10 +86,10 @@ export default class KanbanBoard extends Component {
                         <div className="li-content layout-row justify-content-between align-items-center">
                           <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
                           <div className="icons">
-                            <button disabled={task.stage === Math.min(this.state.tasks[0].stage)} onClick={() => this.moveToBack(task)} className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
+                            <button disabled={task.stage === Math.min(this.stagesNames[0].stage)} onClick={() => this.moveToBack(task)} className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
                               <i className="material-icons">arrow_back</i>
                             </button>
-                            <button disabled={task.stage === Math.max(this.state.tasks[this.state.tasks.length-1].stage)} onClick={() => this.moveToForward(task)} className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
+                            <button disabled={task.stage === Math.max(this.stagesNames[this.stagesNames.length-1].stage)} onClick={() => this.moveToForward(task)} className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
                               <i className="material-icons">arrow_forward</i>
                             </button>
                           </div>
